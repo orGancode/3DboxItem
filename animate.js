fcfa942b679a38622cbb968b8324b91a8d3f27e1
Fei <1065543971@qq.com>
2016-07-29 10:46:58 +0800

    add container origin class,and resolve safari ,because of thier different value of offsetTop ,use DOM element to gain that

--- animate.js
+++ animate.js
@@ -1,64 +1,63 @@
 $(function(){
-		//console.log($('li').eq(0).offset().top);//72
-		//console.log($('li').eq(1).offset().top);//152
-		var uplimit =$('.container').offset().top
-					+parseInt($('.container').css("border-width"))
-					+parseInt($('.container').css("padding-top"))
-					+parseInt($(".container ul").css("padding-top"))
-					-$('li').height();
-		var downlimit=$('.container').offset().top
-					 +$('.container').height()
-					 +parseInt($('.container').css("border-width"))
-					 +parseInt($('.container').css("padding-top"))
-					 +parseInt($(".container ul").css("padding-top"));
-		//init all item
+		var containerT=$('.container').offset().top || document.getElementsByClassName('container')[0].offsetTop;
+		var containerH=document.getElementsByClassName('container')[0].offsetHeight;
+		var uplimit =containerT-document.getElementsByTagName('li')[0].clientHeight;
+		var downlimit=containerH+containerT;
+		
 		function init(){
 			$('li').each(function(){
-				if($(this).offset().top < uplimit || $(this).offset().top > downlimit)
+				var $this=$(this);//var m=$this.get(0)转成dom对象
+				var liTop=$this.get(0).getBoundingClientRect().top;
+				if(liTop < uplimit || liTop > downlimit)//
 					$(this).attr('class',"hidden");
 			});
 		}
 		init();
 
-
-		//defined a timer to be a listener,watching the ul topoffset
 		var ul_timer=setInterval(function(){
-			var ul_top=$('.container ul').offset().top;
+			//getBoundingClientRect().top——js方法获取元素到浏览器顶部距离，offsetTop方法是定值
+			var ul_top=document.getElementsByTagName('ul')[0].getBoundingClientRect().top;
 
 			setTimeout(function(){
-				var u_d=$('.container ul').offset().top-ul_top;
-				if(u_d!=0) scrolll(u_d);
+				var u_d=document.getElementsByTagName('ul')[0].getBoundingClientRect().top-ul_top;
+				if(u_d!=0) {scrolll(u_d);}
 			},10);
 		},10);
 
-
-		//when topoffset changed use this function
 		function scrolll(agr){
-			if(agr>0)//up,scroll up or up key
+			if(agr>0)
 				{
 					$('.container').removeClass('contaiNer').addClass('contaiMer');
 					$('li[class="hidden"]').each(function(){
-						if($(this).offset().top>=uplimit)
-							$(this).addClass('shang').removeClass('hidden');
+						var $this=$(this);
+						var liTop=$this.get(0).getBoundingClientRect().top;
+						if(liTop>=uplimit)
+						$(this).addClass('shang').removeClass('hidden');
 					});
 
 					$('li').each(function(){
-						if($(this).offset().top>=downlimit) $(this).attr('class','hidden');
+						var $this=$(this);
+						var liTop=$this.get(0).getBoundingClientRect().top;
+						if(liTop>=downlimit) $(this).attr('class','hidden');
 					});
 				}
 
 			if(agr<0)//down
-				{
-					$('.container').removeClass('contaiMer').addClass('contaiNer');
-					$('li[class="hidden"]').each(function(){
-						if($(this).offset().top<=downlimit)
-							$(this).addClass('xia').removeClass('hidden');
-					});
+			{
+				$('.container').removeClass('contaiMer').addClass('contaiNer');
+				$('li[class="hidden"]').each(function(){
+					var $this=$(this);
+					var liTop=$this.get(0).getBoundingClientRect().top;
+					if(liTop<=downlimit)
+					$(this).addClass('xia').removeClass('hidden');
+				});
 
-					$('li').each(function(){
-						if($(this).offset().top<=uplimit) $(this).attr('class','hidden');
-					});
-				}
+				$('li').each(function(){
+					var $this=$(this);
+					var liTop = $this.get(0).getBoundingClientRect().top;
+					if(liTop <= uplimit) $(this).attr('class','hidden');
+				});
+			}
 		}
 
 	})
