956e87be3a65d8e6c82ebe63628c54bc3867d874
Fei <1065543971@qq.com>
2016-07-29 10:57:23 +0800

    this cool style support ff chrome ie safari and opera,there are some reason:First, construction of Html ,used be div>ul>li before,but now,i use contain>div>span,div=span,is a single container;secondly,the size of different browsers are different,i have resolved it yestoday

--- animate.js
+++ animate.js
@@ -1,63 +1,56 @@
 $(function(){
-		var containerT=$('.container').offset().top || document.getElementsByClassName('container')[0].offsetTop;
-		var containerH=document.getElementsByClassName('container')[0].offsetHeight;
-		var uplimit =containerT-document.getElementsByTagName('li')[0].clientHeight;
-		var downlimit=containerH+containerT;
-		
-		function init(){
-			$('li').each(function(){
-				var $this=$(this);//var m=$this.get(0)转成dom对象
-				var liTop=$this.get(0).getBoundingClientRect().top;
-				if(liTop < uplimit || liTop > downlimit)//
-					$(this).attr('class',"hidden");
-			});
-		}
-		init();
+    var containerT=$('.contain').offset().top || document.getElementsByClassName('contain')[0].offsetTop;
+    var containerH=document.getElementsByClassName('contain')[0].offsetHeight;
+    var uplimit =document.getElementsByTagName('div')[1].offsetTop;
+    var downlimit=containerH+containerT;
+    //alert(uplimit+"|"+downlimit);
+    function init(){
+      $('span').each(function(){
+        var $this=$(this);//var m=$this.get(0)转成dom对象
+        var spanTop=$this.get(0).getBoundingClientRect().top;
+        if(spanTop < uplimit || spanTop > downlimit)//
+          $(this).attr('class',"hidden");
+       
+      });
+    }
+    init();
 
-		var ul_timer=setInterval(function(){
-			//getBoundingClientRect().top——js方法获取元素到浏览器顶部距离，offsetTop方法是定值
-			var ul_top=document.getElementsByTagName('ul')[0].getBoundingClientRect().top;
+    var span_timer=setInterval(function(){
+      //getBoundingCspanentRect().top——js方法获取元素到浏览器顶部距离，offsetTop方法是定值
+      var span0_top=document.getElementsByTagName('span')[0].getBoundingClientRect().top;
+      setTimeout(function(){
+        var u_d=document.getElementsByTagName('span')[0].getBoundingClientRect().top-span0_top;
+        if(u_d!=0) {scrolll(u_d);}
+      },10);
+    },10);
 
-			setTimeout(function(){
-				var u_d=document.getElementsByTagName('ul')[0].getBoundingClientRect().top-ul_top;
-				if(u_d!=0) {scrolll(u_d);}
-			},10);
-		},10);
+    function scrolll(agr){
+      if(agr < 0){//down
+              $('span[class="hidden"]').each(function(){
+                    var $this=$(this);
+                    var spanTop=$this.get(0).getBoundingClientRect().top;
+                    if(spanTop<downlimit)
+                       $(this).addClass('downscroll').removeClass('hidden');
+                });
+                $('span').each(function(){
+                    var $this=$(this);
+                    var spanTop=$this.get(0).getBoundingClientRect().top;
+                    if(spanTop<=uplimit) $(this).attr('class','hidden');
+                });
+          }
+      if(agr > 0){
+              $('span[class="hidden"]').each(function(){
+                  var $this=$(this);
+                  var spanTop=$this.get(0).getBoundingClientRect().top;
+                  if(spanTop>uplimit)
+                      $(this).addClass('upscroll').removeClass('hidden');
+              });
+              $('span').each(function(){
+                  var $this=$(this);
+                  var spanTop=$this.get(0).getBoundingClientRect().top;
+                  if(spanTop>=downlimit) $(this).attr('class','hidden');
+              });
+          }
+    }
 
-		function scrolll(agr){
-			if(agr>0)
-				{
-					$('.container').removeClass('contaiNer').addClass('contaiMer');
-					$('li[class="hidden"]').each(function(){
-						var $this=$(this);
-						var liTop=$this.get(0).getBoundingClientRect().top;
-						if(liTop>=uplimit)
-						$(this).addClass('shang').removeClass('hidden');
-					});
-
-					$('li').each(function(){
-						var $this=$(this);
-						var liTop=$this.get(0).getBoundingClientRect().top;
-						if(liTop>=downlimit) $(this).attr('class','hidden');
-					});
-				}
-
-			if(agr<0)//down 
-			{
-				$('.container').removeClass('contaiMer').addClass('contaiNer');
-				$('li[class="hidden"]').each(function(){
-					var $this=$(this);
-					var liTop=$this.get(0).getBoundingClientRect().top;
-					if(liTop<=downlimit)
-					$(this).addClass('xia').removeClass('hidden');
-				});
-
-				$('li').each(function(){
-					var $this=$(this);
-					var liTop = $this.get(0).getBoundingClientRect().top;
-					if(liTop <= uplimit) $(this).attr('class','hidden');
-				});
-			}
-		}
-
-	})
+  })
